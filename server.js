a4e32fb6b974ce111cb6aa5f0b626ddcd39148f9
Zain Zafar <zain@aurorasolutions.io>
2018-10-04 17:32:38 +0500

    redirect to success html by default

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const exphbs  = require('express-handlebars');
 const querystring = require('querystring');
 
-const PORT = 8080;
+const PORT = 5000;
 
 const app = express();
 app.use('/static', express.static('static'));
@@ -10,6 +10,12 @@ app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 app.get('/success.html', function (req, res) {
+  const {state, code} = req.query;
+  const q = querystring.stringify({state, code});
+  res.render('success', { appLink: 'https://appsite-redirect.herokuapp.com/app.html?' + q });
+});
+
+app.get('/', function (req, res) {
   const {state, code} = req.query;
   const q = querystring.stringify({state, code});
   res.render('success', { appLink: 'https://appsite-redirect.herokuapp.com/app.html?' + q });
